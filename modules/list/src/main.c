6c4b07e68ef85fe78186e5319505c0c6db6567ca
poshtiTolstoy <tolstihtimur@yandex.ru>
2024-05-30 21:11:03 +0300

    Change makefile

--- modules/list/src/main.c
+++ modules/list/src/main.c
@@ -2,6 +2,12 @@
 #include "../include/tim_list.h"
 
 int main(void) {
-	printf("Hello world!\n");
+	List list;
+	init_list(&list);
+	for (int i = 0; i < 10; ++i) {
+		push(&list, i + 2);
+	}
+	print_list(&list);
+	free_list(&list);
 	return 0;
 }
